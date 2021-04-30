c9bed7a1e39a1f7cf394b04208dbbb2ee27f7bb7
jathanpierce0712 <70153154+jathanpierce0712@users.noreply.github.com>
2021-04-30 10:02:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    $('#text').on('keyup', event => {
+      $('.preview').html($(event.currentTarget).val());
+    });
+    
+    $('#font').on('change', event => {
+      $('.preview').css('fontFamily', $(event.currentTarget).val());
+    });
+    
+    $('#weight').on('change', event => {
+      $('.preview').css('fontWeight', $(event.currentTarget).val());
+    });
+    
+    $('#size').on('keyup', event => {
+      var fontSize = ($(event.currentTarget).val()) + 'px';
+      $('.preview').css('fontSize', fontSize);
+    });
+    
+  })
